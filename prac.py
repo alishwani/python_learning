5d503b81cdbe7529b451c49b0105fa2e5cdf957a
alishwani <33989965+alishwani@users.noreply.github.com>
2022-06-12 00:42:15 -0400

    for-loop

--- prac.py
+++ prac.py
@@ -16,13 +16,10 @@ for fruit in fruits:
 
 for i in change:
     print("I got %r" %i)
-
-    elements = []
-
-    for i in range(0,6):
-        print("Adding %d to the list." %i)
-        elements.append(i)
-
-    for i in elements:
-        print("Element was: %d" %i)
+elements = []
+for i in range(0,6):
+    print("Adding %d to the list." %i)
+    elements.append(i)
+for i in elements:
+    print("Element was: %d" %i)
         
